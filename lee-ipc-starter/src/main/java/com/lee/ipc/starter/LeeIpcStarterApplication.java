caac77128787178a0629c26b5848087f664b7d35
lee <a6265391@qq.com>
2025-06-25 20:11:54 +0800

    first commit

---
+++ lee-ipc-starter/src/main/java/com/lee/ipc/starter/LeeIpcStarterApplication.java
@@ -0,0 +1,13 @@
+package com.lee.ipc.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeIpcStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeeIpcStarterApplication.class, args);
+    }
+
+}
